70440df9a67b5cdea55fa37296f2cb80c4822136
kader01agi <md.abdul.kader.ru@gmail.com>
2023-12-18 13:44:13 +0600

    OK with models.py

--- projectName/polls/views.py
+++ projectName/polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello,World! You are at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
